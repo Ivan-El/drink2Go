96fd04ecec8234452101579167905d1e7ba2497c
Иван <lostnemo@rambler.ru>
2022-11-17 12:49:46 +0600

    Добавляет скрипт открытия меню и переключения страниц

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,23 @@
-/* в этот файл добавляет скрипты*/
+const menuButton = document.querySelector(".main-header__navigation-toggle");
+
+document.body.classList.remove("no-js");
+
+if (menuButton) {
+  menuButton.addEventListener("click", () => {
+    document.body.classList.toggle("menu-open-js");
+    menuButton.classList.toggle("main-header__navigation-toggle--opened");
+  });
+}
+
+const navList = document.querySelector(".site-navigation");
+
+if (navList) {
+  navList.addEventListener("click", (evt) => {
+    const curTarget = evt.currentTarget;
+    const { target } = evt;
+    if (target.classList.contains("site-navigation__link")) {
+      curTarget.querySelector(".site-navigation__link--current").classList.remove("site-navigation__link--current");
+      target.classList.add("site-navigation__link--current");
+    }
+  });
+}
